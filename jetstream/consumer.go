13cf5b688cc7d9c5998478b874c694c0fe474ac9
Mathieu Post <mathieupost@gmail.com>
2023-08-06 20:44:41 +0200

    feat(jetstream): add 2pc stub functions

--- jetstream/consumer.go
+++ jetstream/consumer.go
@@ -14,6 +14,18 @@ import (
 	"github.com/mathieupost/jetflow"
 )
 
+const (
+	HEADER_KEY_MESSAGE_TYPE         = "MESSAGE_TYPE"
+	HEADER_KEY_TRANSACTION_OPERATOR = "TRANSACTION_OPERATOR"
+
+	MESSAGE_TYPE_OPERATOR_CALL = ""
+	MESSAGE_TYPE_PREPARE       = "PREPARE"
+	MESSAGE_TYPE_VOTE_YES      = "VOTE_YES"
+	MESSAGE_TYPE_VOTE_NO       = "VOTE_NO"
+	MESSAGE_TYPE_COMMIT        = "COMMIT"
+	MESSAGE_TYPE_ROLLBACK      = "ROLLBACK"
+)
+
 type Consumer struct {
 	id        uuid.UUID
 	jetstream jetstream.JetStream
@@ -51,7 +63,21 @@ func (c *Consumer) initConsumer(ctx context.Context) error {
 	log.Println("Consumer.initConsumer", consumer.CachedInfo().Name)
 
 	_, err = consumer.Consume(func(msg jetstream.Msg) {
-		go c.handleOperatorCall(msg)
+		msgType := msg.Headers().Get(HEADER_KEY_MESSAGE_TYPE)
+		switch msgType {
+		case MESSAGE_TYPE_OPERATOR_CALL:
+			go c.handleOperatorCall(msg)
+		case MESSAGE_TYPE_PREPARE:
+			go c.handlePrepare(msg)
+		case MESSAGE_TYPE_VOTE_YES:
+			go c.handleVoteYes(msg)
+		case MESSAGE_TYPE_VOTE_NO:
+			go c.handleVoteNo(msg)
+		case MESSAGE_TYPE_COMMIT:
+			go c.handleCommit(msg)
+		case MESSAGE_TYPE_ROLLBACK:
+			go c.handleRollback(msg)
+		}
 	})
 	if err != nil {
 		return errors.Wrap(err, "execute consumer")
@@ -96,6 +122,8 @@ func (c *Consumer) handleOperatorCall(msg jetstream.Msg) {
 		result = operator.Call(ctx, c.client, call)
 
 		if callID == initialCallID {
+			involvedOperators := involvedOperatorsFromCtx(ctx)
+			c.prepareOperators(*involvedOperators)
 			// TODO prepare and commit all operators involved in this request.
 		}
 		success = c.storage.Prepare(ctx, operatorType, operatorID, requestID)
@@ -126,4 +154,32 @@ func (c *Consumer) handleOperatorCall(msg jetstream.Msg) {
 	if callID == initialCallID {
 		log.Println("Consumer published msg", res.Subject, "involvedOperators", involvedOperators)
 	}
+}
+
+func (c *Consumer) prepareOperators(involvedOperators []string) {
+	for _, operator := range involvedOperators {
+		prepareMsg := nats.NewMsg(operator)
+		// TODO send prepare messages to operators.
+		_ = prepareMsg
+	}
+}
+
+func (c *Consumer) handlePrepare(msg jetstream.Msg) {
+	// TODO handle prepare message and respond with a vote.
+}
+
+func (c *Consumer) handleVoteYes(msg jetstream.Msg) {
+	// TODO handle vote yes message and respond with a commit if it was the last one.
+}
+
+func (c *Consumer) handleVoteNo(msg jetstream.Msg) {
+	// TODO handle vote no message and respond with a rollback.
+}
+
+func (c *Consumer) handleCommit(msg jetstream.Msg) {
+	// TODO commit the operator version.
+}
+
+func (c *Consumer) handleRollback(msg jetstream.Msg) {
+	// TODO delete the operator version.
 }
