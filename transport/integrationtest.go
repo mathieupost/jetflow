d72fd80440a4eb0b750fb8ade024a7772d9fd2d5
Mathieu Post <mathieupost@gmail.com>
2023-08-16 14:04:06 +0200

    feat(test): calculate average operation time

--- transport/integrationtest.go
+++ transport/integrationtest.go
@@ -6,11 +6,13 @@ import (
 	"math/rand"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/mathieupost/jetflow"
 	"github.com/mathieupost/jetflow/example/types"
+	"github.com/mathieupost/jetflow/log"
 )
 
 func IntegrationTest(t *testing.T, ctx context.Context, client jetflow.OperatorClient) {
@@ -19,13 +21,20 @@ func IntegrationTest(t *testing.T, ctx context.Context, client jetflow.OperatorC
 	dist := rand.NewZipf(r, 1.5, 1, 100)
 
 	var wg sync.WaitGroup
+	var times sync.Map
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
+		i := i
+		id1 := fmt.Sprintf("user%d", dist.Uint64())
+		id2 := fmt.Sprintf("user%d", dist.Uint64())
+
 		go func() {
 			defer wg.Done()
-
-			id1 := fmt.Sprintf("user%d", dist.Uint64())
-			id2 := fmt.Sprintf("user%d", dist.Uint64())
+			defer func(t time.Time) {
+				duration := time.Now().Sub(t)
+				times.Store(i, duration)
+				log.Println("took", duration)
+			}(time.Now())
 
 			var user1 types.User
 			err := client.Find(ctx, id1, &user1)
@@ -43,4 +52,14 @@ func IntegrationTest(t *testing.T, ctx context.Context, client jetflow.OperatorC
 		}()
 	}
 	wg.Wait()
+	count := 0
+	total := time.Duration(0)
+	times.Range(func(key, value any) bool {
+		count++
+		total = total + value.(time.Duration)
+		return true
+	})
+
+	average := total / time.Duration(count)
+	log.Println("Average:", average)
 }
